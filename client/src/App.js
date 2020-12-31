74bafa7addf5be60640c81098baeeb428eff7a85
kim-hyeonguk <hyeonguk.dev@gmail.com>
2021-01-01 02:35:39 +0900

    Update: MenuBar Home,Login,Register componets

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,23 @@
-import './App.css';
-import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import 'semantic-ui-css/semantic.min.css';
- 
+import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import "semantic-ui-css/semantic.min.css";
+import { Container } from "semantic-ui-react";
 
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import MenuBar from "./components/MenuBar";
 
 function App() {
   return (
     <Router>
-      <Route exact path='/' component={Home} />
-      <Route exact path='/login' component={Login} />
-      <Route exact path='/register' component={Register} />
-      
-
+      <Container>
+        <MenuBar />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/register" component={Register} />
+      </Container>
     </Router>
   );
 }
