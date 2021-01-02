25ccfac1c7bd1d35ddffad4a2443dcaa7e822c05
kim-hyeonguk <hyeonguk.dev@gmail.com>
2021-01-03 05:44:48 +0900

    Update: Client login,logout,register localstorage jwtToken redirect

--- client/src/components/MenuBar.js
+++ client/src/components/MenuBar.js
@@ -1,22 +1,31 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import { Menu } from "semantic-ui-react";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
+import { AuthContext } from "../context/auth";
 
 function MenuBar() {
+  const { user, logout } = useContext(AuthContext);
   const pathName = window.location.pathname;
-  const path = pathName === '/' ? 'home' : pathName.substr(1);
+  const path = pathName === "/" ? "home" : pathName.substr(1);
   const [activeItem, setActiveItem] = useState("home");
 
   const handleItemClick = (e, { name }) => setActiveItem(name);
 
-  return (
+  const menubar = user ? (
+    <Menu pointing secondary size="massive" color="teal">
+      <Menu.Item name={user.userName} active as={Link} to="/" />
+      <Menu.Menu position="right">
+        <Menu.Item name="logout" onClick={logout} />
+      </Menu.Menu>
+    </Menu>
+  ) : (
     <Menu pointing secondary size="massive" color="teal">
       <Menu.Item
         name="home"
         active={activeItem === "home"}
         onClick={handleItemClick}
         as={Link}
-        to="/" 
+        to="/"
       />
       <Menu.Menu position="right">
         <Menu.Item
@@ -36,6 +45,8 @@ function MenuBar() {
       </Menu.Menu>
     </Menu>
   );
+
+  return menubar;
 }
 
 export default MenuBar;
